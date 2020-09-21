9d3db5dd820aea62b6628b947f74c4303908d5e7
David Styrbjörn <davidstyrbjorn@gmail.com>
2020-09-21 11:23:41 +0200

    Matlab whitdrawals

---
+++ image_converter.m
@@ -0,0 +1,7 @@
+function freq_list = image_converter(filePath, min_freq, max_freq)
+
+    img = imgread(filePath);
+    
+    return img;
+    
+end
--- jeff.m
+++ jeff.m
@@ -1,10 +1,11 @@
 
-
-amp=10 ;
+clear all;
+amp=37;
 fs=20500;  % sampling frequency
 duration=1;
-freq=100;
+freq=50;
 values=0:1/fs:duration;
-amp_f= sin(values)
-%a=amp*sin(2*pi* freq*values);
+amp_f= sin(values);
+a=amp*sin(2*pi* freq*values);
 sound(a);
+

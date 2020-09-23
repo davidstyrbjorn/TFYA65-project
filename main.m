dcf8c5a6876f8d8a843c6272b7d1aa07c2fb5a27
David Styrbjörn <davidstyrbjorn@gmail.com>
2020-09-23 14:00:36 +0200

    Dragging Uma in

--- main.m
+++ main.m
@@ -6,26 +6,46 @@ gray_img = rgb2gray(bild)/255;
 img_size = size(gray_img);
 
 min = 10;
-max = 1000;
+max = 2000;
 
-list_freq = zeros(1, img_size(1));
+freq_list = zeros(1, img_size(1));
 
 width = img_size(2);
 height = img_size(1);
 
 for x = 1:width
+   total_freq = 0;
    for y = 1:height
-       if gray_img(y,x) == 1
-           ratio = (height-y)/height;
-           if(y == height)
-               ratio = 0
-           elseif(y == 1)
-               ratio = 1
-           end
-           value = min + (max-min)*ratio;
-           list_freq(1,x) = list_freq(1,x) + value;
-       end
+       t = (y-1)/(height-1);
+       total_freq = total_freq + (min+(max-min)*t) * double(gray_img(y,x));
    end
+   freq_list(1, x) = (total_freq);
 end
+
+clear total_freq;
+
+%%
+
+fs = 44100;
+duration = 1;
+t = [0 : 1/fs : duration];
+
+yA = [];
+for i = size(freq_list, 2)
+    yi = sin(2*pi*freq_list(i)*t);
+    temp = cat(1, yA, yi);
+    yA = temp;
+end
+
+%soundsc(yA, fs);
+
+%%
+
+%A = zeros(1,100);
+
+%A(1:50) = ones(1, 50);
+
+
+
 
 
--- test.m
+++ test.m
@@ -1,9 +1,9 @@
 
 clear all;
 amp=37;
-fs=20500;  % sampling frequency
+fs=20500/2;  % sampling frequency
 duration=1;
-freq=50;
+freq=200;
 values=0:1/fs:duration;
 amp_f= sin(values);
 a=amp*sin(2*pi* freq*values);

f63ddd0739410d4e4d1995d0e63bd717eddd0c03
David Styrbjörn <davidstyrbjorn@gmail.com>
2020-09-25 08:47:52 +0200

    Fungi Lenses

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 clear all;
 
-bild = imread("test_bild2.png");
+bild = imread("pixel_heart.png");
 gray_img = rgb2gray(bild)/255;
 
 img_size = size(gray_img);
 
 min = 10;
-max = 2000;
+max = 350;
 
 freq_list = zeros(1, img_size(1));
 
@@ -16,7 +16,7 @@ height = img_size(1);
 for x = 1:width
    total_freq = 0;
    for y = 1:height
-       t = (y-1)/(height-1);
+       t = 1-(y-1)/(height-1);
        total_freq = total_freq + (min+(max-min)*t) * double(gray_img(y,x));
    end
    freq_list(1, x) = (total_freq);
@@ -24,31 +24,22 @@ end
 
 clear total_freq;
 
-%%
+%% 
 
 fs = 44100;
-duration = 1;
+duration = 0.5;
 t = [0 : 1/fs : duration];
 
-
-yA = zeros(1,fs);
-for i = size(freq_list, 2)
+yA = [];
+for i = 1:size(freq_list, 2)
     yi = sin(2*pi*freq_list(i)*t);
     temp = cat(2, yA, yi);
     yA = temp;
-    
 end
 
- soundsc(yA, fs);
-% sound(yA);
+soundsc(yA, fs);
+
 
 %%
 
-%A = zeros(1,100);
-
-%A(1:50) = ones(1, 50);
-
-
-
-
-
+imshow(bild);
